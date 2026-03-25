ae3e7a8cc821e6bb04f3a48246eb6dcae0b7bd6a
Mamii14w <daniworku126@gmail.com>
2026-03-26 02:28:28 +0300

    fixed flag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,10 +33,10 @@ export class AppComponent implements OnInit, OnDestroy{
 
     this.languageService.initLanguage()
 
-    this.titleService.setTitle( "José Andrés | Frontend Developer" );
+    this.titleService.setTitle( "Daniel Worku | Fullstack Developer" );
 
     this.metaService.addTags([
-      {name: 'keywords', content: 'Frontend, software, developer'},
+      {name: 'keywords', content: 'Fullstack, software, developer'},
       {name: 'description', content: 'Con 4 años de experiencia desarrollando sistemas, interfaces, bots y soluciones tecnológicas  para hacer de la web un lugar mejor. En mi trabajo me gusta liderar, proponer y ejecutar ideas, escribir y refactorizar código limpio, reutilizable y escalable.'},
     ]);
 
